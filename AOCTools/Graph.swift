d099d22c33c26225efd8594b2fe796679dafb153
Marc Haisenko <haisenko@equinux.com>
2024-11-18 22:25:16 +0100

    Change Karger's algorithm to Union-Find structure
    Speeds up the algorithm considerably.

--- AOCTools/Graph.swift
+++ AOCTools/Graph.swift
@@ -55,30 +55,63 @@ extension Edge {
 }
 
 
-/// Edge for use with minimum cut Karger's algorithm.
+/// Union-Find structure implementation.
+/// Also called Disjoint-set data structure.
 private
-struct MergableEdge<Vertex: VertexProtocol> {
+struct UnionFind<Vertex: VertexProtocol> {
     
-    let edge: Edge<Vertex>
-    let original: Edge<Vertex>
-    
-    init(edge: Edge<Vertex>) {
-        self.edge = edge
-        self.original = edge
+    /// A node in the forest.
+    class Node {
+        var parent: Vertex
+        var rank: Int
+        
+        init(parent: Vertex, rank: Int) {
+            self.parent = parent
+            self.rank = rank
+        }
     }
     
-    init(edge: Edge<Vertex>, original: Edge<Vertex>) {
-        self.edge = edge
-        self.original = original
+    /// Nodes lookup table.
+    var nodes: [Vertex: Node] = [:]
+    
+    /// Add a vertex to the forest.
+    mutating func makeSet(vertex: Vertex) {
+        nodes[vertex] = Node(parent: vertex, rank: 0)
     }
     
-    func replace(victim: Vertex, survivor: Vertex) -> MergableEdge<Vertex> {
-        if edge.from == victim {
-            return .init(edge: Edge(from: survivor, to: edge.to), original: original)
-        } else if edge.to == victim {
-            return .init(edge: Edge(from: edge.from, to: survivor), original: original)
-        } else {
-            preconditionFailure("Invalid replacement!")
+    /// Find root of a tree.
+    func findSet(vertex: Vertex) -> Vertex {
+        let subset = nodes[vertex]!
+        if subset.parent == vertex {
+            return vertex
+        }
+        
+        let parent = findSet(vertex: subset.parent)
+        // Compress the path. Speeds up finding the root repeatedly.
+        subset.parent = parent
+        return parent
+    }
+    
+    /// Merge two trees.
+    mutating func union(_ vertex1: Vertex, _ vertex2: Vertex) {
+        let root1 = findSet(vertex: vertex1)
+        let root2 = findSet(vertex: vertex2)
+        
+        if root1 == root2 {
+            return
+        }
+        
+        var subset1 = nodes[root1]!
+        var subset2 = nodes[root2]!
+        
+        if subset1.rank < subset2.rank {
+            (subset1, subset2) = (subset2, subset1)
+        }
+        
+        subset2.parent = subset1.parent
+        
+        if subset1.rank == subset2.rank {
+            subset1.rank += 1
         }
     }
     
@@ -88,27 +121,47 @@ struct MergableEdge<Vertex: VertexProtocol> {
 /// Minimum cut calculation using Karger's algorithm.
 ///
 /// - note: This algorithm has a high _probability_ of returning the minimum number of cuts, but
-///   it doesn't do so.
+///   it doesn't always do so.
 public
 func minimumCut<Vertex: VertexProtocol>(
-    vertices: any Sequence<Vertex>,
-    edges: any Sequence<Edge<Vertex>>
+    vertices: any Collection<Vertex>,
+    edges: any Collection<Edge<Vertex>>
 ) -> [Edge<Vertex>] {
     // https://en.wikipedia.org/wiki/Karger%27s_algorithm
-    var remainingEdges = edges.map(MergableEdge.init)
-    var remainingVertices = Array(vertices)
     
-#if DEBUG
-    checkConsistency(vertices: remainingVertices, edges: remainingEdges)
-#endif
+    #if DEBUG
+    checkConsistency(vertices: vertices, edges: edges)
+    #endif
     
-    var rng = SystemRandomNumberGenerator()
-    while remainingVertices.count > 2 {
-        let index = rng.next(upperBound: UInt(remainingEdges.count))
-        contract(edgeIndex: Int(index), vertices: &remainingVertices, edges: &remainingEdges)
+    var remainingVerticesCount = vertices.count
+    var unionFind: UnionFind<Vertex> = UnionFind()
+    for vertex in vertices {
+        unionFind.makeSet(vertex: vertex)
     }
     
-    return remainingEdges.map(\.original)
+    var rng = SystemRandomNumberGenerator()
+    while remainingVerticesCount > 2 {
+        let edge = edges.randomElement(using: &rng)!
+        let root1 = unionFind.findSet(vertex: edge.from)
+        let root2 = unionFind.findSet(vertex: edge.to)
+        
+        guard root1 != root2 else { continue }
+        
+        unionFind.union(root1, root2)
+        remainingVerticesCount -= 1
+    }
+    
+    var result: [Edge<Vertex>] = []
+    for edge in edges {
+        let root1 = unionFind.findSet(vertex: edge.from)
+        let root2 = unionFind.findSet(vertex: edge.to)
+        
+        guard root1 != root2 else { continue }
+        
+        result.append(edge)
+    }
+    
+    return result
 }
 
 
@@ -119,15 +172,18 @@ func minimumCut<Vertex: VertexProtocol>(
 /// cut need to be searched.
 public
 func minimumCut<Vertex: VertexProtocol>(
-    vertices: any Sequence<Vertex>,
-    edges: any Sequence<Edge<Vertex>>,
+    vertices: any Collection<Vertex>,
+    edges: any Collection<Edge<Vertex>>,
     cuts: Int,
     maximumIterations: Int = 100
 ) -> [Edge<Vertex>] {
-    for _ in 0 ..< maximumIterations {
+    for i in 0 ..< maximumIterations {
         let edges = minimumCut(vertices: vertices, edges: edges)
         if edges.count <= cuts {
+            print("Took \(i + 1) attempts")
             return edges
+        } else {
+            print("\(edges.count) cuts")
         }
     }
     
@@ -136,21 +192,22 @@ func minimumCut<Vertex: VertexProtocol>(
 }
 
 
+
 #if DEBUG
 private
 func checkConsistency<Vertex: VertexProtocol>(
-    vertices: [Vertex],
-    edges: [MergableEdge<Vertex>]
+    vertices: any Sequence<Vertex>,
+    edges: any Sequence<Edge<Vertex>>
 ) {
     var verticesSeen: Set<Vertex> = []
     
     for edge in edges {
-        verticesSeen.insert(edge.edge.from)
-        verticesSeen.insert(edge.edge.to)
-        guard vertices.contains(edge.edge.from) else {
+        verticesSeen.insert(edge.from)
+        verticesSeen.insert(edge.to)
+        guard vertices.contains(edge.from) else {
             preconditionFailure("Invalid edge!")
         }
-        guard vertices.contains(edge.edge.to) else {
+        guard vertices.contains(edge.to) else {
             preconditionFailure("Invalid edge!")
         }
     }
@@ -159,44 +216,3 @@ func checkConsistency<Vertex: VertexProtocol>(
     precondition(unknownVertices.isEmpty, "Unknown vertex!")
 }
 #endif
-
-
-private
-func contract<Vertex: VertexProtocol>(
-    edgeIndex: Int,
-    vertices: inout [Vertex],
-    edges: inout [MergableEdge<Vertex>]
-) {
-    let edge = edges.remove(at: edgeIndex)
-    let survivor = edge.edge.from
-    let victim = edge.edge.to
-    
-    guard let victimIndex = vertices.firstIndex(of: victim) else {
-        preconditionFailure("Invalid victim!")
-    }
-    vertices.remove(at: victimIndex)
-    
-    // Need to patch all connections involving the victim. If replacing the connection would
-    // result in a connection from the survivor to the survivor, the connection needs to be
-    // removed.
-    var needReplacement: [Int] = []
-    var selfReferencing: [Int] = []
-    for (index, candidate) in edges.enumerated()
-        where candidate.edge.from == victim || candidate.edge.to == victim
-    {
-        if candidate.edge.from == survivor || candidate.edge.to == survivor {
-            selfReferencing.append(index)
-        } else {
-            needReplacement.append(index)
-        }
-    }
-    
-    for index in needReplacement {
-        let replacement = edges[index].replace(victim: victim, survivor: survivor)
-        edges[index] = replacement
-    }
-    
-    for index in selfReferencing.sorted(by: >) {
-        edges.remove(at: index)
-    }
-}
